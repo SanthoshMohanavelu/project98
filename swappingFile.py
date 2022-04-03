e678cc1b2c289d7b26f21a5efc760b74ac6c8eaf
Mohanavelu <mohanveluk@gmail.com>
2022-04-03 12:48:34 -0500

    update something

--- swappingFile.py
+++ swappingFile.py
@@ -1,18 +1,17 @@
 def swapFiledata():
-    fileName = input('Enter the file name to swap them..')
-    file = open(fileName, 'r')      
-    for swap in file:    
-        with open(file1, 'r')  as a:    
-            data_a = a.read()   
-    
-        with open(file2, 'r') as b:
-            data_b = b.read()
+    file1 = input('Enter the file name to swap them.. ')
+    file2 = input('Enter the file name to swap them.. ')
+          
+    with open(file1, 'r')  as a:    
+        data_a = a.read()   
+    with open(file2, 'r') as b:
+        data_b = b.read()
 
-        with open(file1, 'w') as a:
-            a.write(data_b)
+    with open(file1, 'w') as a:
+        a.write(data_b)
         
-        with open(file2, 'w') as b:
-            b.write(data_a)
+    with open(file2, 'w') as b:
+        b.write(data_a)
 
 
 swapFiledata()
